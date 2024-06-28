d6847213f82b3f77ae663c49230d88f9b127dec0
william baker <68308394+strayon3@users.noreply.github.com>
2024-06-27 21:30:19 -0500

    Add files via upload

---
+++ dateDetection.py
@@ -0,0 +1,106 @@
+import re
+import pyperclip
+
+
+#TOdo: Get text from clipboard
+counter = 0
+text = str(pyperclip.paste()) 
+mathces = []
+rejected = []
+checks = True
+days = ""
+month = ""
+year = ""
+
+#todo: set date format to search for and numbers to look for  dd/mm/yyyy
+dateRegex = re.compile(r'''\d{1,2}\/\d{1,2}\/\d{2,4}''')#This just looks for the patern
+#you will have to check it to make sure it is right 
+
+for dates in dateRegex.findall(text):
+    for i in dates:
+        #counter to track where you are in string it is +1 indexed 
+        counter+=1
+        if counter <= 2 and counter != 3:
+            days += i
+        if counter == 4 or counter == 5:
+            month += i
+        elif counter > 6:
+            year += i
+    if counter > 9:
+        checks = True
+
+#todo:comfirm that the date is valid
+    dayscheck = int(days)
+    mothcheck = int(month)
+    yearcheck = int(year)
+    leapyear = yearcheck % 4
+
+def ValidDate(days,month,year):
+  days = dayscheck
+  month = mothcheck
+  year = yearcheck
+  if days <= 31 and month <=12 and year <= 2999:
+    return True
+  else:
+    return False
+
+
+def ValidMonthdayCheck(month,date):
+    month = mothcheck
+    date = dayscheck
+
+    if month == 4 or month == 6 or month == 9 or month == 10:
+         if date <= 30:
+              return True
+         else:
+              return False
+         
+def Leapcheck (leap,days,month):
+    days = dayscheck
+    month = mothcheck
+    leap = leapyear
+    if month == 4:
+         
+        if leap == 0:
+             if days <= 29:
+                  return True
+             elif days > 29:
+                  return False
+        else:
+         if leap != 0:
+              if days <=28:
+                   return True
+              else:
+                   return False
+    
+    
+         
+
+    while checks == True:
+        if dayscheck <= 31 and mothcheck <= 12 and yearcheck <= 2999:
+             checks = True
+        if mothcheck == 4 or mothcheck == 6 or mothcheck == 9 or mothcheck == 10 and dayscheck >= 30:
+                        checks = True
+        elif mothcheck == 4 or mothcheck == 6 or mothcheck == 9 or mothcheck == 10 and dayscheck <= 31:
+                        rejected.append(dates)
+                        counter = 0
+                        checks = False
+                        days = ""
+                        month = ""
+                        year = ""
+
+
+        else:
+                mathces.append(dates)
+                checks = False
+                counter = 0
+                days = ""
+                month = ""
+                year = ""
+
+
+        
+
+#todo:Display the matches in a good fashion and copy to clipboard 
+for i in mathces:
+    print(f'{i}')
